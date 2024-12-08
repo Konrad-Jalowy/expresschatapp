3b96ac36b0757ae9a10b8c851b0f95c2f79c478c
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-08 11:48:46 +0100

    minor change

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+      });
+      
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
