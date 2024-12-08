511ab38184d1bd4a9d76493014700624ab9bd088
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-08 10:51:04 +0100

    static and css added

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+
 app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render("chat");
